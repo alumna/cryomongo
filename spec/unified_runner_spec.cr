2f3b920ff715bd907eaad5ff7c93a6dda1569fc2
Paulo Coghi <paulo@airsailer.io>
2026-07-15 12:34:28 +0200

    fix: correctly access properties from Mongo::Commands::Common::BaseResult

--- spec/unified_runner_spec.cr
+++ spec/unified_runner_spec.cr
@@ -3,13 +3,11 @@ require "./unified_runner"
 
 describe "Unified Test Runner" do
   it "bootstraps the environment successfully" do
-    # This just ensures our test suite compiles and runs the docker container
     client = Mongo::Client.new(ENV["MONGODB_URI"])
 
-    # Send a ping to our Mongo 8.0 Docker container to verify connection
     response = client.command(Mongo::Commands::Ping)
     response.should_not be_nil
-    response.try &.["ok"].should eq(1.0)
+    response.not_nil!.ok.should eq(1.0) # <== Access the property directly
 
     client.close
   end
