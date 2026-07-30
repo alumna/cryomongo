bec35da0dd16e3083868cae4cc993ba45d0dba70
Paulo Coghi <paulo@airsailer.io>
2026-07-30 11:28:34 +0200

    test/pef: split the tests into N concurrent jobs defined at specs.yml

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -5,4 +5,5 @@ require "../src/cryomongo"
 # Use the environment variable or default to the Docker replica set we created in CI
 ENV["MONGODB_URI"] ||= "mongodb://localhost:27017/?replicaSet=rs0"
 
-Log.setup(:debug)
+# Changed from :debug to :info to significantly reduce STDOUT I/O overhead during tests
+Log.setup(:info)
--- spec/unified_runner_spec.cr
+++ spec/unified_runner_spec.cr
@@ -15,8 +15,18 @@ describe "Unified Test Runner" do
     client.close
   end
 
-  # Recursively generate a test for every JSON file
-  Dir.glob("spec/tests/unified/**/*.json").sort.each do |file|
+  # Gather all JSON files
+  files = Dir.glob("spec/tests/unified/**/*.json").sort
+
+  # If SPEC_SPLIT is provided (e.g. "1%4"), we only run a fraction of the tests.
+  # This allows GitHub Actions to run tests concurrently across multiple jobs.
+  if split = ENV["SPEC_SPLIT"]?
+    part, total = split.split('%').map(&.to_i)
+    files = files.each_with_index.select { |_file, i| i % total == part }.map(&.first)
+  end
+
+  # Recursively generate a test for every JSON file in our current shard
+  files.each do |file|
     it "executes: #{file}" do
       runner = Mongo::Unified::Runner.new(file)
       runner.run
